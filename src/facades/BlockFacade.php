c6a06e29920740fc5cbd801c030517c1ca711f89
Greg Marineau <greg.marineau@devfactory.ch>
2014-12-23 10:28:15 +0100

    Add the facade for the block

---
+++ src/facades/BlockFacade.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Devfactory\Block\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class BlockFacade extends Facade {
+  /**
+   * Get the registered name of the component.
+   *
+   * @return string
+   */
+  protected static function getFacadeAccessor() { return 'block'; }
+}
