e2c7c8edae40fa4922b4b223b24627cae539c4b1
Greg Marineau <greg.marineau@devfactory.ch>
2014-12-24 10:35:53 +0100

    Add the good package name

--- src/composers/ViewComposers.php
+++ src/composers/ViewComposers.php
@@ -8,13 +8,8 @@ class BlockComposer {
   
   public function compose($view) {
     
-    $prefix = rtrim(Config::get('taxonomy::route_prefix'), '.') . '.';
+    $prefix = rtrim(Config::get('block::route_prefix'), '.') . '.';
     
-    $layout = (object) [
-      'extends' => Config::get('taxonomy::config.layout.extends'),
-      'header' => Config::get('taxonomy::config.layout.header'),
-      'content' => Config::get('taxonomy::config.layout.content'),
-    ];
 
     $view->with(['prefix' => $prefix, 'layout' => $layout]);
   }
