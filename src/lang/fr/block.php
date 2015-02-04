c943fbd53de9bed584d4af53a98bf592d6db18bc
Alcindo Da Costa <alcindo.dacosta@devfactory.ch>
2015-02-04 14:15:17 +0100

    Update french translation

--- src/lang/fr/block.php
+++ src/lang/fr/block.php
@@ -1,9 +1,18 @@
 <?php
 
 return array(
-  'title' => 'Titre',
+  'title' => 'Blocks',
   'body' => 'Body',
-  'add' => 'Ajouter',
+  'regions' => 'Regions',
+  'page' => 'Pages',
+  'visibility' => 'Visibilité',
+  'listed' => 'S\'affiche seulement sur les pages listé',
+  'not_listed' => 'S\'affiche seulement sur les pages non listé',
+  'php' => 'S\'affiche par rapport a la condition PHP',
+  'add' => 'Créer un nouveau block',
   'delete' => 'Supprimer',
-  'edit' => 'Modifier',
+  'edit' => 'Editer',
+  'update' => 'Sauvegarder',
+  'empty' => 'Aucun block pour cette région',
+  'disabled' => 'Désactivé'
 );
