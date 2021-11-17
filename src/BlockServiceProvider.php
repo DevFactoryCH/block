74eff6ea6e33077554d768f45ba266d619a3538e
Greg Marineau <greg.marineau@devfactory.ch>
2021-11-17 17:19:06 +0100

    Update the config

--- src/BlockServiceProvider.php
+++ src/BlockServiceProvider.php
@@ -7,37 +7,67 @@ use Illuminate\Support\ServiceProvider;
 class BlockServiceProvider extends ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
-    public function boot()
-    {
-        $this->package('devfactory/block', 'block', __DIR__);
-        include __DIR__ . '/routes.php';
-    }
-
-    /**
      * Register the service provider.
      *
      * @return void
      */
     public function register()
     {
-        $this->app['block'] = $this->app->share(function($app) {
-            return new Block;
+        $configPath = __DIR__ . '/../config/block.php';
+        $this->mergeConfigFrom($configPath, 'block');
+
+        $this->app->bind('block', function($app) {
+            return new Block();
         });
     }
 
     /**
-     * Get the services provided by the provider.
+     * Bootstrap the application events.
      *
-     * @return array
+     * @return void
      */
-    public function provides()
+    public function boot()
     {
-        return ['block'];
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'block');
+
+        $this->publishes([
+            __DIR__.'/../resources/lang' => resource_path('lang/vendor/block'),
+        ]);
+
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'block');
+
+        $this->loadRoutesFrom(realpath(__DIR__ . '/routes.php'));
+    }
+
+    /**
+     * Get the active router.
+     *
+     * @return Router
+     */
+    protected function getRouter()
+    {
+        return $this->app['router'];
+    }
+
+    /**
+     * Get the config path
+     *
+     * @return string
+     */
+    protected function getConfigPath()
+    {
+        return config_path('block.php');
+    }
+
+    /**
+     * Publish the config file
+     *
+     * @param  string $configPath
+     */
+    protected function publishConfig($configPath)
+    {
+        $this->publishes([$configPath => config_path('block.php')], 'config');
     }
 }
