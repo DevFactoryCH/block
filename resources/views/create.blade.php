b60f37fc30747ffc4e7512137726b1a404deb14a
Greg Marineau <greg.marineau@devfactory.ch>
2021-11-18 09:53:11 +0100

    Update the template

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,25 +3,21 @@
 @section('content')
 
   <div class="container">
-    <div class="row">
-      <div class="col-sm-8">
-        <div class="box box-primary">
-          <div class="box-body">
-            {{ Form::open(['route' => 'block.store']) }}
-              <div class="form-group">
-                {{ Form::label('title', __('block::block.title')) }}
-                {{ Form::text('title', null, ['class' =>'form-control']) }}
-                {!! $errors->first('title', '<span class="text-danger">:message</span>') !!}
-              </div>
-              <div class="form-group">
-                {{ Form::label('body', __('block::block.body')) }}
-                {{ Form::textarea('body', null, ['class' => 'form-control']) }}
-                {!! $errors->first('body', '<span class="text-danger">:message</span>') !!}
-              </div>
-              {{ Form::button(__('block::block.add'), ['class' => 'btn btn-primary', 'type' => 'submit']) }}
-            {{ Form::close() }}
+    <div class="card">
+      <div class="card-body">
+        {{ Form::open(['route' => 'block.store']) }}
+          <div class="form-group">
+            {{ Form::label('title', __('block::block.title')) }}
+            {{ Form::text('title', null, ['class' =>'form-control']) }}
+            {!! $errors->first('title', '<span class="text-danger">:message</span>') !!}
           </div>
-        </div>
+          <div class="form-group">
+            {{ Form::label('body', __('block::block.body')) }}
+            {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+            {!! $errors->first('body', '<span class="text-danger">:message</span>') !!}
+          </div>
+          {{ Form::button(__('block::block.add'), ['class' => 'btn btn-primary', 'type' => 'submit']) }}
+        {{ Form::close() }}
       </div>
     </div>
   </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,36 +8,31 @@
         @lang('block::block.add')
       </a>
     </p>
-  
-    <div class="row">
-      <div class="col-sm-8">
-        <div class="box box-primary">
-          <div class="box-body">
-            <table class="table table-stripped">
-              <thead>
-                <tr>
-                  <th>@lang('block::block.title')</th>
-                  <th></th>
-                </tr>
-              </thead>
-              @foreach ($blocks as $block)
-                <tr>
-                  <td>{{ $block->title }}</td>
-                  <td class="text-right ">
-                    {{ Form::open(['route' => ['block.destroy', $block->id], 'method' => 'DELETE', 'class' => 'pull-right']) }}
-                      {{ Form::button(__('block::block.delete'), ['class' => 'btn btn-xs btn-danger', 'type' => 'submit']) }}
-                    {{ Form::close() }}
-                    {{ Form::open(['route' => ['block.edit', $block->id], 'method' => 'GET', 'class' => 'pull-right', 'style' => 'margin-right: 5px']) }}
-                      {{ Form::button(__('block::block.edit'), ['class' => 'btn btn-xs btn-primary', 'type' => 'submit']) }}
-                    {{ Form::close() }}
-                  </td>
-                </tr>
-              @endforeach
-            </table>
-          </div>
-        </div>
+    <div class="card">
+      <div class="card-body">
+        <table class="table table-stripped">
+          <thead>
+            <tr>
+              <th>@lang('block::block.title')</th>
+              <th></th>
+            </tr>
+          </thead>
+          @foreach ($blocks as $block)
+            <tr>
+              <td>{{ $block->title }}</td>
+              <td class="text-right">
+                {{ Form::open(['route' => ['block.destroy', $block->id], 'method' => 'DELETE', 'class' => 'float-end']) }}
+                  {{ Form::button(__('block::block.delete'), ['class' => 'btn btn-sm btn-danger', 'type' => 'submit']) }}
+                {{ Form::close() }}
+                {{ Form::open(['route' => ['block.edit', $block->id], 'class' => 'float-end']) }}
+                  {{ Form::button(__('block::block.edit'), ['class' => 'btn btn-sm btn-primary me-2', 'type' => 'submit']) }}
+                {{ Form::close() }}
+              </td>
+            </tr>
+          @endforeach
+        </table>
       </div>
-    </div>  
+    </div>
   </div>
 
 @stop
